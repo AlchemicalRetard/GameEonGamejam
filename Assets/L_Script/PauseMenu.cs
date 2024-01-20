031c847d7e2c4fda43de5da606c25a8e6e4ea456
Akash K <117154341+im-AkashK@users.noreply.github.com>
2024-01-20 22:20:55 +0530

    PauseMenu

---
+++ Assets/L_Script/PauseMenu.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public Button resumeButton;
+    public Button quitButton;
+    public GameObject target;
+    public static bool isPaused = false;
+
+    void Start()
+    {
+        resumeButton.onClick.AddListener(Resume);
+        quitButton.onClick.AddListener(Quit);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            target.SetActive(!target.activeSelf);
+            isPaused = !isPaused;
+            Time.timeScale = isPaused ? 0 : 1;
+        }
+    }
+
+    void Resume()
+    {
+        target.SetActive(false);
+        isPaused = false;
+        Time.timeScale = 1;
+    }
+
+    void Quit()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
